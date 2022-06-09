1ebe0b6da1dc352fafa059c27ebfae9ee3df420e
Rotimi Awani <rotimi.awani@ibm.com>
2022-06-09 08:29:04 -0700

    Testing week 1 solution

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
+import "./About.css";
+import pic from "../assets/pic.jpg";
   
 export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
+             <div>
+        <div class="split left">
+          <div className="centered">
+            <img 
+              className="profile_image"
+              src={pic}
+              alt="Profile Pic"
+              ></img>
+          </div>
+        </div>
+        <div className="split right">
+          <div className="centered">
+            <div className="name_title">Rotimi Awani</div>
+            <div className="brief_description">
+              I am a software engineer at IBM
+            </div>
+          </div>
+        </div>
+      </div>
       </div>
     )
   }
